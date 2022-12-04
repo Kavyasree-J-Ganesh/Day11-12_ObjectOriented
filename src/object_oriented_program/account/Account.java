9ab67fbdec0a072dca35e68499c143a293dbf914
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-04 22:46:44 +0530

    [Kavyasree J Ganesh] Account Debit problem

---
+++ src/object_oriented_program/account/Account.java
@@ -0,0 +1,20 @@
+package object_oriented_program.account;
+
+public class Account {
+    int accountBalance;                  // Instance variable
+
+    void debitFromBalance(int debitAmount){    // Instance method
+        if(this.accountBalance < debitAmount){
+            System.out.println("Debit amount exceeded account balance");
+        }else{
+           this.accountBalance = this.accountBalance - debitAmount;
+            System.out.println(" Account balance after Amount debit : " + this.accountBalance );
+        }
+    }
+    public static void main(String[] args) {
+        Account sbi = new Account();    // creating the instance of Account class
+        sbi.accountBalance = 25000;   // assigning value to instance variable.
+        sbi.debitFromBalance(10000);
+        sbi.debitFromBalance(20000);
+    }
+}
